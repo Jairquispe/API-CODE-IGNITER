ccaf61021a613ea8b977ca838c81f93c0fcf13de
Jairquispe <jairquispe123456789@gmail.com>
2024-11-20 15:32:46 -0500

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'https://github.com/Jairquispe/API-CODE-IGNITER.git';
+    public string $baseURL = 'https://api-code-igniter.onrender.com';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
