2fa80c7681e3a2cd9de6a69fdee08736d1c5602e
Jairquispe <jairquispe123456789@gmail.com>
2024-11-20 21:19:18 -0500

    Update RestProducto.php

--- app/Controllers/RestProducto.php
+++ app/Controllers/RestProducto.php
@@ -9,12 +9,20 @@ class RestProducto extends ResourceController
     protected $modelName = "App\Models\ProductoModel";
     protected $format = "json";
 
+
+        // Constructor explícito para cargar el modelo
+    public function __construct()
+    {
+        $this->model = new ProductoModel(); // Inicializa el modelo explícitamente
+    }
+
     /**
      * Listar todos los productos
      */
     public function listar_productos()
     {
-        $productos = $this->model->listar_productos();
+        $productoModel = new ProductoModel(); // Crear instancia del modelo
+        $productos = $productoModel->listar_productos(); // Llamar a la función
         return $this->respond($productos);
     }
 
